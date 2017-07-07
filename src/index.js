d30ac51c185009494d64b4fa0d3b47bf890b619f
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-07-07 18:00:34 -0400

    add basic local state management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+/* global localStorage */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import './main.css';
+import { regions } from './Data/regions.json';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ransei = localStorage.getItem('ransei') || {};
+
+regions.forEach((region) => {
+  ransei[region] = ransei[region] || {
+    warriors: [],
+  };
+});
+
+ReactDOM.render(<App ransei={ransei} />, document.getElementById('root'));
 registerServiceWorker();
