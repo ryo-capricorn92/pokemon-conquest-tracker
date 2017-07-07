c9904322e0826c8d9c27e53ecdbfb174b6b242ca
Ryo Wheatley <ryo.capricorn92@gmail.com>
2017-07-07 14:01:07 -0400

    add Header component

---
+++ src/Components/Presentation/Header.jsx
@@ -0,0 +1,10 @@
+import Grid from './Grid';
+
+const Header = Grid.extend`
+  background: darksalmon;
+  border: 2px solid palevioletred;
+  border-radius: 0 0 10px 10px;
+  padding: 20px;
+`;
+
+export default Header;
